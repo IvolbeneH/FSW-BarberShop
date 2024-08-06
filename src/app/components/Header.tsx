c64afc67955dd1c8376b5b817f7626a69c6e81a0
IvolbeneH <ivolbene1@gmail.com>
2024-08-06 15:29:24 -0300

    feat: add Header and banner

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image"
+import { Card, CardContent } from "./ui/card"
+import { Button } from "./ui/button"
+import { MenuIcon } from "lucide-react"
+
+export function Header() {
+  return (
+    <header>
+      <Card>
+        <CardContent className="flex flex-row items-center justify-between p-5">
+          <Image
+            src="/logo.png"
+            alt="Logo FSW Barber"
+            height={18}
+            width={120}
+          />
+          <Button variant="outline" size="icon">
+            <MenuIcon />
+          </Button>
+        </CardContent>
+      </Card>
+    </header>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,31 @@
+import { SearchIcon } from "lucide-react"
+import { Header } from "./components/Header"
+import { Input } from "./components/ui/input"
+import { Button } from "./components/ui/button"
+import Image from "next/image"
+
 export default function Home() {
   return (
     <div>
-      <h1 className="my-6 text-zinc-500">OI TESTE MUNDO</h1>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Ivolbene!</h2>
+        <p>Terça feira, 06 de agosto.</p>
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Search" />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+            alt="Agende nos melhores com FSW Barber"
+          />
+        </div>
+      </div>
     </div>
   )
 }
